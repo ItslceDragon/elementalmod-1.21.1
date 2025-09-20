d95da55bc789858cd50be01e13a385780368bec6
ItsIceDragon <icedragon4371@gmail.com>
2025-09-19 20:30:30 -0600

    Initial commit

---
+++ src/main/java/net/gremi/elementalmod/ElementalMod.java
@@ -0,0 +1,15 @@
+package net.gremi.elementalmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElementalMod implements ModInitializer {
+	public static final String MOD_ID = "elementalmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/gremi/elementalmod/ElementalModClient.java
@@ -0,0 +1,10 @@
+package net.gremi.elementalmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
